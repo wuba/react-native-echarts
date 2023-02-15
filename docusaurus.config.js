e8b8fa920ca0823ee83da798c0ad5d3e2df90238
zhiqingchen <329604487@qq.com>
2023-02-15 10:39:24 +0800

    docs: init i18n

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,7 +29,7 @@ const config = {
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['en', 'zh-Hans'],
   },
 
   presets: [
@@ -80,6 +80,10 @@ const config = {
             label: 'Tutorial',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            type: 'localeDropdown',
+            position: 'right',
+          },
           {
             href: 'https://github.com/wuba/wrn-echarts',
             label: 'GitHub',
