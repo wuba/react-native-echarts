0ed558f24d057ca349bc5b2353690c45c859a160
zhiqingchen <329604487@qq.com>
2023-03-02 19:24:54 +0800

    feat: export echarts

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import * as _echarts from 'echarts/index';
 export { default as SkiaChart, SkiaRenderer } from './skia/skiaChart';
 export { default as SvgChart, SVGRenderer } from './svg/svgChart';
 export * from './types';
+export const echarts = _echarts;
