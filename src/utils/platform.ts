ff955a8d7fadff9a2f5cb0337c3b28ab1c439b9b
zhiqingchen <329604487@qq.com>
2023-03-02 15:02:27 +0800

    chore: remove useless code
    Signed-off-by: zhiqingchen <329604487@qq.com>

--- src/utils/platform.ts
+++ src/utils/platform.ts
@@ -2,7 +2,6 @@
 // under BSD-3-Clause license
 // add some patch for react native
 
-import { platformApi } from 'zrender/lib/core/platform';
 import { DEFAULT_FONT_FAMILY, defaultWidthMapStr } from './font';
 
 const DEFAULT_FONT_SIZE = 12;
@@ -26,34 +25,20 @@ function getTextWidthMap(mapStr: string) {
 const DEFAULT_TEXT_WIDTH_MAP = getTextWidthMap(defaultWidthMapStr);
 
 export const measureText = (function () {
-  let _ctx: any;
-  let _cachedFont: any;
   return function (text: string, font: string) {
-    if (!_ctx) {
-      const canvas = platformApi.createCanvas();
-      _ctx = canvas && canvas.getContext('2d');
-    }
-    if (_ctx) {
-      if (_cachedFont !== font) {
-        _cachedFont = _ctx.font = font || DEFAULT_FONT;
-      }
-      return _ctx.measureText(text);
+    text = text || '';
+    font = font || DEFAULT_FONT;
+    const res = /([0-9]*?)px/.exec(font);
+    const fontSize = res && res[1] ? +res[1] : DEFAULT_FONT_SIZE;
+    let width = 0;
+    if (font.indexOf('mono') >= 0) {
+      width = fontSize * text.length;
     } else {
-      text = text || '';
-      font = font || DEFAULT_FONT;
-      const res = /([0-9]*?)px/.exec(font);
-      const fontSize = res && res[1] ? +res[1] : DEFAULT_FONT_SIZE;
-      let width = 0;
-      if (font.indexOf('mono') >= 0) {
-        width = fontSize * text.length;
-      } else {
-        for (var i = 0; i < text.length; i++) {
-          var preCalcWidth = DEFAULT_TEXT_WIDTH_MAP[text[i] as string];
-          width += preCalcWidth == null ? fontSize : preCalcWidth * fontSize;
-        }
+      for (var i = 0; i < text.length; i++) {
+        var preCalcWidth = DEFAULT_TEXT_WIDTH_MAP[text[i] as string];
+        width += preCalcWidth == null ? fontSize : preCalcWidth * fontSize;
       }
-
-      return { width: width };
     }
+    return { width: width };
   };
 })();
