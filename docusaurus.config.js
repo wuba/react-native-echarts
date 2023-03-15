ef09fa23436f282b2194b079e7f29886143f0eff
zhiqingchen <329604487@qq.com>
2023-03-15 11:59:59 +0800

    fix: trailingSlash false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,6 +26,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'wuba', // Usually your GitHub org/user name.
   projectName: 'react-native-echarts', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
