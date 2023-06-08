4183f32805c7e8a72d8b09b50e97177933e084ee
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2023-06-08 14:11:39 +0200

    feat: improve throttle typing
    Signed-off-by: Guillaume Bonnet <mrsquaare@mrsquaare.fr>

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,6 +1,11 @@
-export function throttle(func: Function, wait: number) {
+export type ThrottledFunction<T extends any[]> = (...args: T) => void;
+
+export function throttle<T extends any[]>(
+  func: ThrottledFunction<T>,
+  wait: number
+) {
   let lastExecution = 0;
-  return (...args: any) => {
+  return (...args: T) => {
     const now = Date.now();
     if (now - lastExecution >= wait) {
       lastExecution = now;
