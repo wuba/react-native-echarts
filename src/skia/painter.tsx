af96c95d489ce08a45ef99dd2c73c6c594c713b6
tony chen <tony.zq.chen@gmail.com>
2024-08-12 16:04:19 +0800

    feat: support background

--- src/skia/painter.tsx
+++ src/skia/painter.tsx
@@ -6,13 +6,12 @@ import Path from 'zrender/lib/graphic/Path';
 import { PainterBase } from 'zrender/lib/PainterBase';
 import type Storage from 'zrender/lib/Storage';
 import { logError } from 'zrender/lib/core/util';
+import { isGradient, isPattern } from 'zrender/lib/svg/helper';
 import { createBrushScope } from './core';
-import { brush, setClipPath } from './graphic';
+import { brush, setClipPath, setGradient } from './graphic';
 import { ReactElement } from 'react';
-import {
-  Group,
-  Skia,
-} from '@shopify/react-native-skia';
+import { Group, Rect } from '@shopify/react-native-skia';
+import React from 'react';
 let svgId = 0;
 
 interface SVGPainterOption {
@@ -20,6 +19,7 @@ interface SVGPainterOption {
   height?: number;
   ssr?: boolean;
 }
+type SVGPainterBackgroundColor = string | GradientObject | PatternObject;
 
 interface RootProps extends HTMLElement {
   getChartSize: () => {
@@ -37,6 +37,8 @@ export class SkiaPainter implements PainterBase {
   private _width: number;
   private _height: number;
   private _svgDom: any;
+  private _backgroundColor: SVGPainterBackgroundColor = 'none';
+  private _bgVNode: ReactElement | undefined;
   constructor(
     root: RootProps,
     storage: Storage,
@@ -56,9 +58,9 @@ export class SkiaPainter implements PainterBase {
   getType() {
     return this.type;
   }
-  setBackgroundColor(
-    backgroundColor: string | GradientObject | PatternObject
-  ): void {}
+  setBackgroundColor(backgroundColor: SVGPainterBackgroundColor): void {
+    this._backgroundColor = backgroundColor;
+  }
   getViewportRoot: () => HTMLElement = () => {
     return this.root as HTMLElement;
   };
@@ -69,7 +71,16 @@ export class SkiaPainter implements PainterBase {
   refresh(): void {
     const scope = createBrushScope(this._id);
     const list = this.storage.getDisplayList(true);
+    const width = this._width;
+    const height = this._height;
     let children: ReactElement[] = [];
+    const bgVNode = (this._bgVNode = createBackgroundVNode(
+      width,
+      height,
+      this._backgroundColor,
+      scope
+    ));
+    bgVNode && children.push(bgVNode);
     this._paintList(list, scope, children);
     // @ts-ignore
     this.root.elm.patch(children);
@@ -84,54 +95,64 @@ export class SkiaPainter implements PainterBase {
     for (const displayable of list) {
       if (!displayable.invisible) {
         const clipPaths = displayable.__clipPaths;
-        const len = clipPaths && clipPaths.length || 0;
-        const prevLen = prevClipPaths && prevClipPaths.length || 0;
+        const len = (clipPaths && clipPaths.length) || 0;
+        const prevLen = (prevClipPaths && prevClipPaths.length) || 0;
         let lca;
         // Find the lowest common ancestor
         for (lca = Math.max(len - 1, prevLen - 1); lca >= 0; lca--) {
-            if (clipPaths && prevClipPaths
-                && clipPaths[lca] === prevClipPaths[lca]
-            ) {
-                break;
-            }
+          if (
+            clipPaths &&
+            prevClipPaths &&
+            clipPaths[lca] === prevClipPaths[lca]
+          ) {
+            break;
+          }
         }
         // pop the stack
         for (let i = prevLen - 1; i > lca; i--) {
-            clipPathsGroupsStackDepth--;
-            // svgEls.push(closeGroup);
-            currentClipPathGroup = clipPathsGroupsStack[clipPathsGroupsStackDepth - 1];
+          clipPathsGroupsStackDepth--;
+          // svgEls.push(closeGroup);
+          currentClipPathGroup =
+            clipPathsGroupsStack[clipPathsGroupsStackDepth - 1];
         }
         // Pop clip path group for clipPaths not match the previous.
         for (let i = lca + 1; i < len; i++) {
-            const groupAttrs: any = {};
-            setClipPath(
-                clipPaths[i],
-                groupAttrs,
-                scope
-            );
-            const g = {
-              clip: groupAttrs['clip-path'],
-              children: [],
-              index: out?.length,
-            };
-            clipPathsGroupsStack[clipPathsGroupsStackDepth++] = g;
-            outGroups.push(g);
-            currentClipPathGroup = g;
+          const groupAttrs: any = {};
+          setClipPath(clipPaths[i], groupAttrs, scope);
+          const g = {
+            id: 'clip-g-' + clipGroupNodeIdx++,
+            clip: groupAttrs['clip-path'],
+            children: [],
+            index: out?.length,
+          };
+          clipPathsGroupsStack[clipPathsGroupsStackDepth++] = g;
+          outGroups.push(g);
+          currentClipPathGroup = g;
         }
         prevClipPaths = clipPaths;
         const ret = brush(displayable, scope);
         if (ret) {
           if (ret instanceof Array) {
-            (currentClipPathGroup ? currentClipPathGroup.children : out)?.push(...ret);
+            (currentClipPathGroup ? currentClipPathGroup.children : out)?.push(
+              ...ret
+            );
           } else {
-            (currentClipPathGroup ? currentClipPathGroup.children : out)?.push(ret);
+            (currentClipPathGroup ? currentClipPathGroup.children : out)?.push(
+              ret
+            );
           }
         }
       }
     }
     for (let i = outGroups.length - 1; i >= 0; i--) {
       const group = outGroups[i];
-      out?.splice(group.index, 0, <Group clip={group.clip}>{group.children}</Group>);
+      out?.splice(
+        group.index,
+        0,
+        <Group key={group.id} clip={group.clip}>
+          {group.children}
+        </Group>
+      );
     }
   }
   clear(): void {}
@@ -168,4 +189,35 @@ function createMethodNotSupport(method: string): any {
       logError('In SVG mode painter not support method "' + method + '"');
     }
   };
+}
+
+function createBackgroundVNode(
+  width: number,
+  height: number,
+  backgroundColor: SVGPainterBackgroundColor,
+  _scope: BrushScope
+) {
+  let bgVNode;
+  if (backgroundColor && backgroundColor !== 'none') {
+    const attrs: Record<string, string | number | boolean> = {};
+    if (isGradient(backgroundColor)) {
+      setGradient({ fill: backgroundColor as any }, attrs, 'fill');
+    } else if (isPattern(backgroundColor)) {
+      // todo
+    } else {
+      attrs.fill = backgroundColor;
+    }
+    if (typeof attrs.fill === 'string') {
+      return (
+        <Rect x={0} y={0} width={width} height={height} color={attrs.fill} />
+      );
+    } else {
+      return (
+        <Rect x={0} y={0} width={width} height={height}>
+          {attrs.fill}
+        </Rect>
+      );
+    }
+  }
+  return bgVNode;
 }
