643df087554565fb2ad548a5fabaea9a639c36a0
tony chen <tony.zq.chen@gmail.com>
2024-11-06 13:22:46 +0800

    chore: lint fix

--- src/skia/helper.ts
+++ src/skia/helper.ts
@@ -1,4 +1,4 @@
 const EPSILON = 1e-4;
 export function isAroundZero(transform: number) {
-    return transform < EPSILON && transform > -EPSILON;
+  return transform < EPSILON && transform > -EPSILON;
 }
